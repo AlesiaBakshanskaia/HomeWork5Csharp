4ea4c14e064433e4de10e633bd7d21811b2c65d7
Alesia <andomyel@yandex.ru>
2022-10-17 22:17:32 +0300

    HomeWork5C#Task1

---
+++ HW5C#/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] Array34 = GetArray(10, 100, 999);
+Console.WriteLine(String.Join(" ", Array34));
+Console.WriteLine($"Количество четных числе в массиве {CountEven(Array34)}");
+
+
+//Метод подсчета четных чисел в массиве
+int CountEven (int[] array){
+    int count = 0;
+    foreach (int element in array){
+        if (element%2 == 0){
+            count += 1;
+        }
+    }
+    return count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Метод задания массива
+int[] GetArray (int size, int min, int max){
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++){
+        res[i] = new Random().Next(min, max+1);
+    }
+    return res;
+}
