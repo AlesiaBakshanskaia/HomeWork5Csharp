aae260b965a6fe80ae7b31ab885cb4dbdc204ba7
Alesia <andomyel@yandex.ru>
2022-10-17 22:47:38 +0300

    HomeWork5C#Task2

--- HW5C#/Program.cs
+++ HW5C#/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
-int[] Array34 = GetArray(10, 100, 999);
+/*int[] Array34 = GetArray(10, 100, 999);
 Console.WriteLine(String.Join(" ", Array34));
 Console.WriteLine($"Количество четных числе в массиве {CountEven(Array34)}");
 
@@ -14,12 +14,33 @@ int CountEven (int[] array){
     }
     return count;
 }
+*/
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] Array36 = GetArray(9, -100, 100);
+Console.WriteLine(String.Join(" ", Array36));
+Console.WriteLine($"Сумма чисел на нечетных позициях в массиве {SumEvenPosition(Array36)}");
+
+//Метод поиска суммы элементов на нечетных позициях массива
+int SumEvenPosition (int[] array){
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2){
+        sum += array[i];
+    }
+    return sum;    
+    }
 
 
-
-
-
-
+//Метод поиска суммы элементов на нечетных позициях массива. 2 вариант
+/*int SumEvenPosition (int[] array){
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++){
+        if (i % 2 != 0){
+            sum += array[i];
+        }
+    }
+    return sum;    
+    }*/
 
 
 
